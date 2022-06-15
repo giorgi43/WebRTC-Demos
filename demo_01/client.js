871db0a8b18f4f64f69f40c79a74e8a4b9189847
giorgi43 <1234567gpu@gmail.com>
2022-06-15 17:51:57 +0400

    modify

--- demo_01/client.js
+++ demo_01/client.js
@@ -1,4 +1,4 @@
-let signalingServer = new WebSocket('ws://localhost:8080');
+let signalingServer = new WebSocket('ws://192.168.1.16:8080');
 
 let username = document.querySelector('#username');
 let connectBtn = document.querySelector('#connectBtn');
@@ -12,8 +12,14 @@ connectBtn.addEventListener('click', function(e) {
 
 signalingServer.onmessage = function(message) {
     console.log("Got message:", message.data);
-}
 
+    var data = JSON.parse(message.data);
+
+    console.log(data);
+    // switch (message.data.type) {
+    //     case 'login'
+    // }
+}
 
 function sendMessage(message) {
     signalingServer.send(JSON.stringify(message))
